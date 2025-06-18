0321c5f15d5d2e25c9b75629fdea4c5e32b6a5d9
Eric Gosnell <eric.gosnell@colorado.edu>
2025-06-18 10:40:13 -0600

    refactor(sdr->sdr.hpp): create sdr class header
    moving sdr-related variable definitions from main.cpp into a new class
    
    not working as header is not yet used, need to create sdr.cpp

--- sdr/main.cpp
+++ sdr/main.cpp
@@ -45,36 +45,6 @@ void sig_int_handler(int) {
 
 /*** CONFIGURATION PARAMETERS ***/
 
-// DEVICE
-string device_args;
-string subdev;
-string clk_ref;
-double clk_rate;
-string tx_channels; 
-string rx_channels;
-string cpu_format;
-string otw_format;
-
-// GPIO
-int pwr_amp_pin;
-string gpio_bank;
-uint32_t AMP_GPIO_MASK;
-uint32_t ATR_MASKS;
-uint32_t ATR_CONTROL;
-uint32_t GPIO_DDR;
-int ref_out_int;
-
-// RF
-double rx_rate;
-double tx_rate;
-double freq;
-double rx_gain;
-double tx_gain;
-double bw;
-string tx_ant;
-string rx_ant;
-bool transmit;
-
 // CHIRP
 double time_offset;
 double tx_duration;
