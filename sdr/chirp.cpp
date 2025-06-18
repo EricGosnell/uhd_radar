7ab6b03bc4b030c31ba6002702baf1014ff866a3
ediegoldner <edie.goldner@colorado.edu>
2025-06-18 15:24:07 -0600

    refactor(sdr): Add chirp.hpp and chirp.cpp
    refactor chirp member variables and functions into their own library to clean up main.cpp
    
    not called from main yet

---
+++ sdr/chirp.cpp
@@ -0,0 +1,37 @@
+#include "chirp.hpp"
+
+YAML::Node chirp = config["CHIRP"];
+
+/**
+ * @brief constructs a new Chirp object
+ * 
+ * creates new Chirp object and calls function that initializes each member variable from yaml file 
+ * @param kYamlFile Path to the YAML configuration file (config/)
+ */
+
+Chirp::Chirp(const string& kYamlFile){
+    assignVarFromYaml(kYamlFile);
+}
+
+/**
+ * @brief assigns all member variables from YAML file
+ * 
+ * reads in paramaters from the YAML file in the config/ folder and assigns paramaters to variables in Chirp class
+ * @param kYamlFile Path to the YAML configuration file (config/)
+ */
+void Chirp::assignVarFromYaml(const string& kYamlFile){
+    YAML::Node config = YAML::LoadFile(kYamlFile);
+    YAML::Node chirp = config["CHIRP"];
+
+    time_offset = chirp["time_offset"].as<double>();
+    tx_duration = chirp["tx_duration"].as<double>();
+    rx_duration = chirp["rx_duration"].as<double>();
+    tr_on_lead = chirp["tr_on_lead"].as<double>();
+    tr_off_trail = chirp["tr_off_trail"].as<double>();
+    pulse_rep_int = chirp["pulse_rep_int"].as<double>();
+    tx_lead = chirp["tx_lead"].as<double>();
+    num_pulses = chirp["num_pulses"].as<int>();
+    num_presums = chirp["num_presums"].as<int>(1); // Default of 1 is equivalent to no pre-summing
+    phase_dither = chirp["phase_dithering"].as<bool>(false);
+
+}
