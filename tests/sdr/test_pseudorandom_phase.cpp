84e82dec8ad39919b3f1a017ebf95ae272152187
Colinj <colinjameschow@gmail.com>
2025-06-24 14:01:08 -0600

    test(tests.sdr.test_pseudorandom): pseudorandom Unit Tests

--- tests/sdr/test_pseudorandom_phase.cpp
+++ tests/sdr/test_pseudorandom_phase.cpp
@@ -6,3 +6,14 @@ TEST(PseudorandomPhase, ReturnValidFloat) {
     EXPECT_NO_THROW(get_next_phase(true));
     EXPECT_NO_THROW(get_next_phase(false));
 }
+
+TEST(GetNPhases, VectorOfFloats) {
+    int n = 10;
+    auto result = get_next_n_phases(n, true);
+    EXPECT_EQ(result.size(), n);
+}
+
+TEST(GetNPhases, TestForZeroN) {
+    auto result = get_next_n_phases(0, true);
+    EXPECT_TRUE(result.empty());
+}
