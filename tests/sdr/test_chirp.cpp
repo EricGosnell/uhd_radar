921e034155bba56bea82470615c305f8063836b6
ediegoldner <edie.goldner@colorado.edu>
2025-06-25 10:01:10 -0600

    test(tests/sdr/): Add unit test for chirp class
    Checks to make sure all the default info from the  yaml file is correctly assinged in assignVarFromYaml in the chirp class

--- tests/sdr/test_chirp.cpp
+++ tests/sdr/test_chirp.cpp
@@ -1,2 +1,20 @@
 #include "../../sdr/chirp.hpp"
 #include <gtest/gtest.h>
+
+using namespace std;
+
+TEST(assignVarFromYaml, loadsDefault){
+    string yaml_filename = "../../../config/default.yaml";
+    Chirp chirp(yaml_filename);
+
+    EXPECT_EQ(chirp.time_offset, 1);
+    EXPECT_EQ(chirp.tx_duration, 20e-6);
+    EXPECT_EQ(chirp.rx_duration, 20e-6);
+    EXPECT_EQ(chirp.tr_on_lead, 0e-6);
+    EXPECT_EQ(chirp.tr_off_trail, 0e-6);
+    EXPECT_EQ(chirp.pulse_rep_int, 200e-6);
+    EXPECT_EQ(chirp.tx_lead, 0e-6);
+    EXPECT_EQ(chirp.num_pulses, 10000);
+    EXPECT_EQ(chirp.num_presums, 1);
+    EXPECT_EQ(chirp.phase_dither, true);
+}
