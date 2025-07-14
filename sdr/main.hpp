680c985898fd50a821adac6aa5fdf6060025e4a7
Eric Gosnell <eric.gosnell@colorado.edu>
2025-07-14 11:07:53 -0600

    fix(sdr): re-add include path for std::filesystem
    main.hpp did not have the new filesystem library

--- sdr/main.hpp
+++ sdr/main.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "common.hpp"
-
 #include <uhd/utils/thread.hpp>
 #include <uhd/utils/safe_main.hpp>
 #include <uhd/exception.hpp>
@@ -16,6 +14,7 @@
 #include <complex>
 #include <mutex>
 #include <cstdlib>
+#include <filesystem>
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/posix/stream_descriptor.hpp>
 #include <boost/asio/write.hpp>
@@ -26,6 +25,7 @@
 #include "utils.hpp"
 #include "sdr.hpp"
 #include "chirp.hpp"
+#include "common.hpp"
 
 void transmit_worker(tx_streamer::sptr& tx_stream, rx_streamer::sptr& rx_stream, Chirp& chirp, Sdr& sdr);
 void handleRxBuffer(size_t n_samps_in_rx_buff, rx_metadata_t& rx_md, Chirp& chirp, vector<complex<float>>& buff, vector<complex<float>>& sample_sum, float& inversion_phase);
