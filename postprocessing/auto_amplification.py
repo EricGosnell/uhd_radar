63361eafec41823809a2d0c5e5fa135788e9999d
ediegoldner <edie.goldner@colorado.edu>
2025-07-22 15:16:23 -0600

    feat(postprocessing): continuing to develop the automatic amplification file
    still developing functionality for this file and need to test to see if it works
    
    this file is not fully finished yet

--- postprocessing/auto_amplification.py
+++ postprocessing/auto_amplification.py
@@ -4,6 +4,7 @@ import numpy as np
 import scipy.signal as sp
 import processing as pr
 import matplotlib.pyplot as plt
+import generate_chirp as gen 
 from ruamel.yaml import YAML as ym
 
 c = 3e8
@@ -26,8 +27,7 @@ with open(args.yaml_file) as stream:
 
    factors = config["ENVIRONMENTAL_FACTORS"]
    
-   transmit_power = reference_power + tx_gain - loss
-   #TODO: use gps data to calculate radius for geometric spreading
+   #TODO: use gps data to calculate radius(altitude) for geometric spreading
    radius = 100 # should be changed later, this is control data
    wavelength = (c/freq)
    geometric_spreading = ((wavelength)/(4*pi*radius))^2
@@ -37,34 +37,38 @@ with open(args.yaml_file) as stream:
    specular_reflection_coeff_current = factors["specular_reflection_coeff_current"]
    antenna_gain = factors["antenna_gain"]
    tilt_of_plane = factors["tilt_of_plane"]
+   max_rx_gain = factors["max_rx_gain"]
+   reference_power = factors["reference_power"]
+   power_loss = factors["power_loss"]
+   transmit_power = reference_power + tx_gain - power_loss #measurements in .yaml file
 
 print("Config factors and parameters being uploaded...")
 
 bool = True
 while(bool):
-   #transmit_chirps
+   gen.generate_chirp(config) #generates the chirp that the data is taken from
    rx_sig = pr.loadSamplesFromFile(args.rx_samps_file, config)
    max_rx_volts = np.max(rx_sig)
    max_rx_samp_power = ((max_rx_volts)^2)/50 #R is 50 ohms
 
-   current_rx_compute_power = transmit_power*antenna_gain*geometric_spreading*scattering_current*specular_reflection_coeff_current #using radar power equation with varibales from config
-   max_rx_compute_power = transmit_power*antenna_gain*geometric_spreading*scattering_max*specular_reflection_coeff_max #using radar power equation with varibales from config
+   current_rx_compute_power = transmit_power*antenna_gain*geometric_spreading*scattering_current*specular_reflection_coeff_current #using radar power equation with current varibales from config
+   max_rx_compute_power = transmit_power*antenna_gain*geometric_spreading*scattering_max*specular_reflection_coeff_max #using radar power equation with max varibales from config
    
    dB_transmit_power = 10*np.log10(transmit_power)
    dB_current_power = 10*np.log10(current_rx_compute_power) + 10
    dB_max_power = 10*np.log10(max_rx_compute_power) + 10
    
-   db_gain = dB_max_power/dB_transmit_power#calculate gain based on above samples
+   db_gain = dB_max_power/dB_transmit_power #calculate gain based on above samples
 
-   if(dB_gain > 76): #76 dB is the max availabe rx gain from b205 mini
+   if(dB_gain > max_rx_gain): #76 dB is the max availabe rx gain for b205 mini
       raise ValueError("Calculated rx gain is too high for b205 mini")
    #catch error?
 
-   
+
    #generate graphs using new gain based on scales power
    #output graphs and new rx_gain
 
-   print("Are you satisfied with this new gain? 'yes' or 'no'")
+   print("New gain is", dB_gain, "dB. Are you satisfied with this new gain? 'yes' or 'no'")
    user_ans = input().lower()
    if(user_ans = 'yes'):
       bool = False
